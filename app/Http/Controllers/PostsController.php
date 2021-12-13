86e2777c4a7f765086dfd54bcef4b3f6e7e20189
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2021-12-13 23:02:45 +0800

    練習8-1串接posts路由之postscontroller的index方法

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::orderBy('created_at','DESC')->get();
+        $data = ['posts'=>$posts];
+        return view ('posts.index',$data);
     }
 
     public function show($id)
